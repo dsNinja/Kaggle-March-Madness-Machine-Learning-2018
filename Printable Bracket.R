9647b494b05e83cb2aa7ed1fdec16bbf8165ed86
Yi Zhang <yizhanghh@yahoo.com>
2022-03-13 00:47:16 -0500

    Adj. data folders. Ran thru 2022 w mockup stage2.

--- Printable Bracket.R
+++ Printable Bracket.R
@@ -3,14 +3,13 @@ devtools::install_github('zachmayer/kaggleNCAA')
 
 rm(list=ls())
 
-setwd('March Madness 2018')
-file = ('stage2_submission1.csv')
+file = './2022/stage2_submission2.csv'
 pred = read.csv(file = file, head=TRUE, sep=",")
 
 set.seed(1)
 library('kaggleNCAA')
 dat <- parseBracket(file, w=0) 
-sim <- simTourney(dat, 1000, progress=TRUE)
+sim <- simTourney(dat, 3, progress=TRUE, w=0)
 
 bracket <- extractBracket(sim)
 printableBracket(bracket) 
